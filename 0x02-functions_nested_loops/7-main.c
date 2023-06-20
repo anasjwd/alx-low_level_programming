f7f434f3cced0a118b8c92449d1d5b953e72140d
anasjwd <anasjawad7777@gmail.com>
2023-06-20 16:05:04 -0700

    task7 v.2

--- 0x02-functions_nested_loops/7-main.c
+++ 0x02-functions_nested_loops/7-main.c
@@ -7,7 +7,12 @@
  */
 int main(void)
 {
+	int r;
+
 	print_last_digit(98);
-	print_last_digit(-1024);
+	print_last_digit(0);
+	r = print_last_digit(-1024);
+	_putchar(r + '0');
+	_putchar('\n');
 	return (0);
 }
