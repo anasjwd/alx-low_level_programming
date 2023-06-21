42c7099e8858eb6ae4bf6164ab64c755c16ae6c6
anasjwd <anasjawad7777@gmail.com>
2023-06-21 09:11:42 -0700

    task0 v.2

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: A C program generates n
+ *
+ * Return: 0 on success
+ */
+int positive_or_negative(int n)
+{
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else
+		printf("%d is negative\n", n);
+	return (0);
+}
