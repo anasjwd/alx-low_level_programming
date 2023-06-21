c08aeac6ed820bcbfb974be69b688b9f5aeabc47
anasjwd <anasjawad7777@gmail.com>
2023-06-21 11:27:25 -0700

    task0 v.3

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -2,19 +2,19 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main - Entry point of the program
+ * positive_or_negative - Entry point of the program
  *
- * @n: the integer
+ * @i: integer to test
  *
  * Return: 0 on success
  */
-int positive_or_negative(int n)
+int positive_or_negative(int i)
 {
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n == 0)
-		printf("%d is zero\n", n);
+	if (i > 0)
+		printf("%d is positive\n", i);
+	else if (i == 0)
+		printf("%d is zero\n", i);
 	else
-		printf("%d is negative\n", n);
+		printf("%d is negative\n", i);
 	return (0);
 }
