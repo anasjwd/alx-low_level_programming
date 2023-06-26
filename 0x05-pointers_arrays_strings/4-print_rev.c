b205cd3c520397475777d19a4d0cb645e010329b
anasjwd <anasjawad7777@gmail.com>
2023-06-26 02:58:51 -0700

    task4 v.2

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,10 +7,13 @@
 
 void print_rev(char *s)
 {
-	int i;
+	int i = 0;
 
-	for (i = 62 ; i > -1 ; i--)
-		_putchar(*(s + i));
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	for (i -= 1; i >= 0 ; i--)
+		_putchar(s[i]);
 	_putchar('\n');
-
 }
