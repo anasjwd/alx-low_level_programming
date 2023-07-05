7eef68f26a5872a4441e59efe34c2879878f003a
anasjwd <anasjawad7777@gmail.com>
2023-07-05 11:03:08 -0700

    task5 v.3

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -12,7 +12,7 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	return (actual_sqrt_recursion(n, 0));
+	return (second_sqrt_recursion(n, 0));
 }
 
 /**
@@ -28,5 +28,5 @@ int second_sqrt_recursion(int n, int i)
 		return (i);
 	if (i * i > n)
 		return (-1);
-	return  (actual_sqrt_recursion(n, i + 1));
+	return  (second_sqrt_recursion(n, i + 1));
 }
