d3201887a1ce5ac0582c6c5c79e38d140fca489a
anasjwd <anasjawad7777@gmail.com>
2023-07-05 11:00:05 -0700

    task5 v.1

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - original function
+ * @n: the number
+ * Return: the second function
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (actual_sqrt_recursion(n, 0));
+}
+
+/**
+ * second_sqrt_recursion - second function
+ * @n: the number
+ * @i: fa2r tajarib
+ * Return: the natural square root
+ */
+
+int second_sqrt_recursion(int n, int i)
+{
+	if (i * i == n)
+		return (i);
+	if (i * i > n)
+		return (-1);
+	return  (actual_sqrt_recursion(n, i + 1));
+}
