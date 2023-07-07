5b42aa0d3edde3a5f466e0051c53fb822be4db31
anasjwd <anasjawad7777@gmail.com>
2023-07-07 08:55:07 -0700

    task0 v.1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name, followed by a new line.
+ * @argc: The size of the argv array
+ * @argv: The size of the argv array
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
