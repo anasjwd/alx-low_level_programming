6b1dcd22301ba1e24c2ab30042406a6a321872fd
anasjwd <anasjawad7777@gmail.com>
2023-07-10 04:04:12 -0700

    task0 v.3

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,10 +10,17 @@
 char *create_array(unsigned int size, char c)
 {
 	char *p;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
 
 	p = malloc(sizeof(char) * size);
 	if (p == 0)
 		return (NULL);
-	p[0] = c;
+
+	for (i = 0 ; i < size ; i++)
+		p[i] = c;
+
 	return (p);
 }
