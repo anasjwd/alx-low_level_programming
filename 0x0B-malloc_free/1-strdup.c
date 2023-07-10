a8fa20bbd0186153e6d35c6f6ffa16e2d2f47108
anasjwd <anasjawad7777@gmail.com>
2023-07-10 05:16:21 -0700

    task1 v.5

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -14,7 +14,7 @@ char *_strdup(char *str)
 	if (str == NULL)
 		return (NULL);
 
-	p = malloc(sizeof(char) * strlen(str));
+	p = malloc(sizeof(char) * (strlen(str) + 1));
 	if (p == NULL)
 		return (NULL);
 
