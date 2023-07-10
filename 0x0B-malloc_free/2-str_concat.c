389121f8dd5521f735417cbf64e75a9e1cb60685
anasjwd <anasjawad7777@gmail.com>
2023-07-10 06:29:36 -0700

    task2 v.2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,20 +10,26 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	int length = strlen(s1) + strlen(s2) + 1;
+	int length;
 	char *s3;
 
-	/*allocate dynamically a space for s1 and s2*/
+	/*treating NULL as an empty string*/
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	length = strlen(s1) + strlen(s2) + 1;
+
+	/*allocating dynamically a space for s1 and s2*/
 	s3 = malloc(sizeof(char) * length);
 	/*if no space memory is left return NULL to avoid any segfault*/
 	if (s3 == NULL)
 		return (NULL);
 
-	/*cocatenate the tweo string in s3*/
-	strcat(s3, s1);
+	/*cocatenating the tweo string in s3*/
+	strcpy(s3, s1);
 	strcat(s3, s2);
-	/*add the null terminated to the end of the string*/
-	s3[length] = '\0';
 
 	return (s3);
 }
