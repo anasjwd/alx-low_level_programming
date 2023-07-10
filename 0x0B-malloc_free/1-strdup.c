f6cc04aa041b73933777c68cdff16035fe659f11
anasjwd <anasjawad7777@gmail.com>
2023-07-10 05:05:10 -0700

    task1 v.1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strdup - copy of the string given as a parameter.
+ * @str: pointer to the string
+ * Return: pointer to the duplicated string
+ */
+
+char *_strdup(char *str)
+{
+	int length = strlen(str) + 1;
+	char *p;
+
+	if (str == NULL)
+		return (NULL);
+
+	p = malloc(sizeof(*str) * length);
+	if (p == NULL)
+		return (NULL);
+	strcpy(p, str);
+
+	return (p);
+}
