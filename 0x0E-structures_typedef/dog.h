808b05818a580557c96fcdaa1146394ea65ce1e5
anasjwd <anasjawad7777@gmail.com>
2023-07-17 09:09:28 -0700

    task0 v.2

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - dog's infos.
+ * @name: the name of the dog.
+ * @age: the age of the dog.
+ * @owner: the name of the owner of the dog.
+ */
+
 struct dog
 {
 	char *name;
