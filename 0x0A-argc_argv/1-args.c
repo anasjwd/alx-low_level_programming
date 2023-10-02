f4a12eeb8f4e9d91cf5719b31cb4dcd440fbf982
anasjwd <anasjawad7777@gmail.com.com>
2023-10-02 00:55:39 -0700

    program that prints the number of arguments passed into it.

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * putnbr - print a number.
+ * @d: the number.
+ */
+void putnbr(int d)
+{
+	if (d < 10)
+	{
+		_putchar(d + '0');
+		return;
+	}
+	putnbr(d / 10);
+	_putchar(d % 10 + '0');
+}
+
+/**
+ * main - program that prints the number of arguments passed into it.
+ * @argc: number of command line arguments.
+ * @argv: array of command line arguments.
+ *
+ * Return: 0.
+ */
+int main(int argc, char **argv)
+{
+	(void)argv;
+
+	putnbr(argc);
+	_putchar('\n');
+	return (0);
+}
