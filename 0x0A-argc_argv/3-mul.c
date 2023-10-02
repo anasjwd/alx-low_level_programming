f1d6e50c482aae89428f14c79d92b494c94b9fa3
anasjwd <anasjawad7777@gmail.com.com>
2023-10-02 02:11:29 -0700

    program that multiplies two numbers.

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,69 @@
+#include "main.h"
+#include <unistd.h>
+
+int _atoi(char *str)
+{
+	int i;
+	int sign;
+	int result;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+		{
+			sign *= -1;
+			i++;
+		}
+		else if (str[i] == '+')
+		{
+			i++;
+		}
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result *= 10;
+		result += str[i] - '0';
+		i++;
+	}
+	return (result * sign);
+}
+
+/**
+ * putnbr - print a number.
+ * @d: the number.
+ */
+void putnbr(int d)
+{
+	if (d < 10)
+	{
+		_putchar(d + '0');
+		return;
+	}
+	putnbr(d / 10);
+	_putchar(d % 10 + '0');
+}
+
+/**
+ * main - program that multiplies two numbers..
+ * @argc: number of command line arguments.
+ * @argv: array of command line arguments.
+ *
+ * Return: 0.
+ */
+int main(int argc, char **argv)
+{
+	int mult;
+
+	if (argc == 3)
+	{
+		mult = _atoi(argv[1]) * _atoi(argv[2]);
+		putnbr(mult);
+		_putchar('\n');
+		return (0);
+	}
+	write(1, "Error\n", 6);
+	return (1);
+}
