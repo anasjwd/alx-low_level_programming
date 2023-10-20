22c0c2b003b075d7b7f8d4acfad8726abf434eaa
anasjwd <anasjawad7777@gmail.com>
2023-10-20 09:54:26 -0700

    task4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_list - function that frees a list_t list.
+ * @head: the head.
+ */
+void free_list(list_t *head)
+{
+	list_t *node;
+	list_t *node1;
+
+	node = head;
+	while (node != NULL)
+	{
+		node1 = node;
+		node = node->next;
+		free(node1);
+	}
+}
