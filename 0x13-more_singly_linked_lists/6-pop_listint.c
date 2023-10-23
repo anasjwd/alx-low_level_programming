ed1e8d8b628feab4fd49b74c95c60f8eaf28322c
anasjwd <anasjawad7777@gmail.com>
2023-10-23 13:20:19 -0700

    done

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -8,11 +8,12 @@
  */
 int pop_listint(listint_t **head)
 {
-	listint_t *node = *head;
+	listint_t *node;
 	int n;
 
-	if (head == NULL)
+	if (head == NULL || *head == NULL)
 		return (0);
+	node = *head;
 	n = node->n;
 	*head = node->next;
 	free(node);
