9d244d718d623a51056a31ebc08c3384d991e066
anasjwd <anasjawad7777@gmail.com>
2023-10-23 13:03:05 -0700

    task2

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -12,6 +12,8 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	listint_t *node;
 
 	node = malloc(sizeof(listint_t));
+	if (node == NULL)
+		return (NULL);
 	node->n = n;
 	node->next = *head;
 	*head = node;
