ea1a400923ba6c8524ff3e6815b596b0ddbad12c
anasjwd <anasjawad7777@gmail.com>
2023-10-23 13:40:53 -0700

    done

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,16 +9,13 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	listint_t *node = head;
-	unsigned int i;
+	unsigned int i = 0;
+	listint_t *temp = head;
 
-	if (head == NULL)
-		return (NULL);
-	for (i = 0; i <= (index - 1); i++)
+	while (temp && i < index)
 	{
-		node = node->next;
-		if (node == NULL)
-			return (NULL);
+		temp = temp->next;
+		i++;
 	}
-	return (node);
+	return (temp ? temp : NULL);
 }
