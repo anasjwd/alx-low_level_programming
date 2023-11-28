779d1612b1456d8c2703d47df894837b2ea38f64
anasjwd <anasjawad7777@gmail.com>
2023-11-28 15:15:49 -0800

    reads a text file and prints it to the POSIX standard output.

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,17 +16,28 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (!filename)
 		return (0);
 	fd = open(filename, O_RDONLY);
-	if (fd < 0)
+	if (fd == -1)
 		return (0);
 	buffer = malloc(letters * sizeof(char));
 	if (!buffer)
+	{
+		close(fd);
 		return (0);
+	}
 	bytes_read = read(fd, buffer, letters);
-	if (bytes_read <= 0)
+	if (bytes_read == -1)
+	{
+		free(buffer);
+		close(fd);
 		return (0);
+	}
 	bytes_write = write(STDOUT_FILENO, buffer, bytes_read);
-	if (bytes_write < bytes_read)
+	if (bytes_write != bytes_read)
+	{
+		free(buffer);
+		close(fd);
 		return (0);
+	}
 	free(buffer);
 	close(fd);
 	return (bytes_write);
